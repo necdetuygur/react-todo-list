5dc52a655a0e35aceaa54c0ccfbe3e06eb6a3595
Necdet UYGUR <necdet.uygur@gmail.com>
2021-07-03 22:30:33 +0300

    Core

---
+++ src/Todo.js
@@ -0,0 +1,55 @@
+function ls(key, obj) {
+    if (obj) {
+        localStorage.setItem(key, JSON.stringify(obj));
+    }
+    if (localStorage.getItem(key) != null) {
+        return JSON.parse(localStorage.getItem(key));
+    } else {
+        return [];
+    }
+}
+
+let todos = ls("todos") || [];
+
+const Todo = {
+    "Add": function (todoTitle) {
+        const todo = {
+            id: "TodoID_" + new Date() * 1,
+            title: todoTitle,
+            time: new Date(),
+            completed: false
+        }
+        todos.push(todo);
+        updateTodos();
+    },
+    "Remove": function (todoId) {
+        const index = todos.map(x => {
+            return x.id;
+        }).indexOf(todoId);
+        todos.splice(index, 1);
+        updateTodos();
+    },
+    "GetAll": function () {
+        return ls("todos");
+    },
+    "Edit": function (todoId, todoTitle) {
+        const index = todos.map(x => {
+            return x.id;
+        }).indexOf(todoId);
+        todos[index].title = todoTitle;
+        updateTodos();
+    },
+    "ToggleComplete": function (todoId) {
+        const index = todos.map(x => {
+            return x.id;
+        }).indexOf(todoId);
+        todos[index].completed = !todos[index].completed;
+        updateTodos();
+    }
+};
+
+function updateTodos() {
+    ls("todos", todos)
+}
+
+export default Todo;
