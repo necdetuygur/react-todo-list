8ea3f576619562d92a95bd99eef07657976325dc
Necdet UYGUR <necdet.uygur@gmail.com>
2021-07-03 22:30:48 +0300

    Card

---
+++ src/Components/Card.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class Card extends Component {
+    render() {
+        return (
+            <div className="card">
+                <h5 className="card-header d-flex justify-content-between align-items-center">
+                    {this.props.title}
+                    {this.props.button}
+                </h5>
+                <div className="card-body">
+                    {this.props.children}
+                </div>
+            </div>
+        )
+    }
+}
