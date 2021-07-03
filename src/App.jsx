57d0a0cf1004a1ac0c84bff12d68f4bbd9e0912e
Necdet UYGUR <necdet.uygur@gmail.com>
2021-07-03 22:31:26 +0300

    Finish 🚀

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,63 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import Card from './Components/Card';
+import Button from './Components/Button';
+import Todo from './Todo';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            todos: []
+        };
+    }
+
+    componentDidMount() {
+        this.refreshTodos();
+    }
+
+    refreshTodos() {
+        this.setState({
+            todos: Todo.GetAll()
+        });
+    }
+
     render() {
         return (
-            <div className="container">
-                
+            <div className="container mt-3">
+                <Card title="React Todo List" button={<Button type="primary" onClick={() => { Todo.Add(window.prompt("Todo Title", "")); this.refreshTodos(); }} text="Add New" />}>
+                    <div className="list-group list-group-flush">
+                        {(this.state.todos.length < 1) ? (
+                            <div>
+                                <div>Your todo list is empty ☁️</div>
+                                <div>Press the "Add New" button to add a new todo 🚀</div>
+                            </div>
+                        ) : (this.state.todos.map((todo, key) => (
+                            <div className="list-group-item" key={key}>
+                                <div className="d-flex w-100 justify-content-between">
+                                    <h5 className="mb-1">
+                                        {
+                                            todo.completed ? (
+                                                <del>{todo.title}</del>
+                                            ) : (
+                                                <div>{todo.title}</div>
+                                            )
+                                        }
+                                    </h5>
+                                    <small>{moment(todo.time).format("YYYY-MM-DD HH:mm:ss")}</small>
+                                </div>
+                                <div className="d-flex w-100 justify-content-between">
+                                    <Button type="warning" onClick={() => { Todo.ToggleComplete(todo.id); this.refreshTodos(); }} text={todo.completed ? "Set Incomplete" : "Set Complete"} />
+                                    <Button type="primary" onClick={() => { Todo.Edit(todo.id, prompt("Todo Title", todo.title)); this.refreshTodos(); }} text="Edit" />
+                                    <Button type="danger" onClick={() => { if (window.confirm("Are you sure?")) { Todo.Remove(todo.id); this.refreshTodos(); } }} text="Remove" />
+                                </div>
+                            </div>
+                        )))}
+                    </div>
+                </Card>
             </div>
         );
     }
