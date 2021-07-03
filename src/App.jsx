64ae66196a0e767bdf4166ae2c99f23ee3a4905a
Necdet UYGUR <necdet.uygur@gmail.com>
2021-07-03 20:15:17 +0300

    Clear

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="container">
+                
+            </div>
+        );
+    }
+}
+
+export default App;
